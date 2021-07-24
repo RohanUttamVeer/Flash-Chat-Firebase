141aa0baf94735d5bacf05cbf3414b551ba8424d
RohanUttamVeer <rohanveer04@gmail.com>
2021-07-24 20:44:29 +0530

    animation added

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -9,11 +9,39 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  Animation? animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: Duration(seconds: 5),
+      vsync: this,
+      // upperBound: 60,
+    );
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white).animate(controller);
+
+    controller.forward();
+    controller.addListener(() {
+      setState(() {});
+      print(animation!.value);
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation!.value,
+      // backgroundColor: Colors.deepOrangeAccent.withOpacity(controller!.value),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -27,10 +55,12 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   child: Container(
                     child: Image.asset('assets/chat.png'),
                     height: 60.0,
+                    // height: animation!.value * 70,
                   ),
                 ),
                 Text(
                   ' Flash Chat',
+                  // ' ${controller!.value.toInt()}%',
                   style: TextStyle(
                       fontSize: 45,
                       fontWeight: FontWeight.w900,
